7961ce92fd466240354df2facfa8dc45ecf084b9
Abedalkareem <abedalkareem.omreyh@yahoo.com>
2019-10-28 00:18:35 +0400

    add the automove feature

---
+++ Zaina/Classes/Views/Objects/SceneView.swift
@@ -0,0 +1,21 @@
+//
+//  SceneView.swift
+//  Zaina
+//
+//  Created by abedalkareem omreyh on 27/10/2019.
+//  Copyright © 2019 abedalkareem. All rights reserved.
+//
+
+import UIKit
+
+class SceneView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
